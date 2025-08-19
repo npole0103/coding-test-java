a03512a1e004d8377b5b3d8ca4d78a0af03e342a
npole0103 <npole0103@naver.com>
2025-08-19 18:03:23 +0900

    feat: Add Sort Example

--- src/Sort.java
+++ src/Sort.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Student {
+    String name;
+    int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
 public class Sort {
+    public static void main(String[] args) {
+        List<Student> students = Arrays.asList(
+                new Student("Kim", 90),
+                new Student("Lee", 85),
+                new Student("Park", 90),
+                new Student("Choi", 70)
+        );
+
+        students.sort((a, b) -> {
+            if(a.score < b.score) {
+                return a.name.compareTo(b.name);
+            }
+
+            return Integer.compare(b.score, a.score);
+        });
+
+        for(Student s : students) {
+            System.out.println(s.name + " " + s.score);
+        }
+    }
 }
