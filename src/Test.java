520c3f33d7e075fc4b0ea9886193d2c5db9d690d
npole0103 <npole0103@naver.com>
2025-08-20 00:57:27 +0900

    feat: Add Test class

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        String aa = "abcd";
+        String bb = "ab";
+        String[] arr = new String[0];
+
+        Arrays.sort(arr);
+        
+        aa.contains(bb);
+    }
+}
