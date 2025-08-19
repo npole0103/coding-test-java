fe50dcd39f1fa8b898436ece30c61e10432312c6
npole0103 <npole0103@naver.com>
2025-08-19 18:03:38 +0900

    feat: Add Greedy Example

--- src/Greedy.java
+++ src/Greedy.java
@@ -1,2 +1,19 @@
+import java.util.Arrays;
+
 public class Greedy {
+    public static void main(String[] args) {
+        int[][] meetings = {{1, 4}, {3, 5}, {0, 6}, {5, 7}, {3, 8}, {8, 9}};
+
+        Arrays.sort(meetings, (a, b) -> Integer.compare(a[1], b[1])
+        );
+
+        int count = 0, end = 0;
+        for(int[] m : meetings) {
+            if(m[0] >= end) {
+                count++;
+                end = m[1];
+            }
+        }
+        System.out.println("최대 회의 수 : " + count);
+    }
 }
